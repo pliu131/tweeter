285ab374acde51b866dfd6a6ae2994dee20cb968
Peter Liu <pliu7@binghamton.edu>
2014-01-08 02:26:10 -0500

    implemented signin and signout

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,11 @@ module UsersHelper
 		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
+
+  def sign_in(user)
+  	remember_token = User.new_remember_token
+  	cookies.permanent[:remember_token] = remember_token
+  	user.update_attribute(:remember_token, User.encrypt(remember_token))
+  	self.current_user = user
+  end
 end
